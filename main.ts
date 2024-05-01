9899d6ca7ca5a52ef811a921a294278fc3ff5390
ElectroBoticsIndia <168177858+ElectroBoticsIndia@users.noreply.github.com>
2024-05-01 18:34:32 +0530

    Corrected the sound and logic for stop

--- main.ts
+++ main.ts
@@ -7,8 +7,8 @@ radio.onReceivedNumber(function (receivedNumber) {
             # . . . #
             . # # # .
             `)
-        servos.P0.setAngle(180)
-        servos.P2.setAngle(0)
+        servos.P0.setAngle(0)
+        servos.P2.setAngle(180)
         music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.UntilDone)
     }
     if (receivedNumber == 2) {
@@ -16,18 +16,24 @@ radio.onReceivedNumber(function (receivedNumber) {
             # # . # #
             # # . # #
             . . . . .
-            . # # # .
             # . . . #
+            . # # # .
             `)
-        servos.P0.setAngle(0)
-        servos.P2.setAngle(180)
+        servos.P0.setAngle(180)
+        servos.P2.setAngle(0)
         music.play(music.builtinPlayableSoundEffect(soundExpression.sad), music.PlaybackMode.UntilDone)
     }
     if (receivedNumber == 3) {
-        basic.showIcon(IconNames.Heart)
-        music.play(music.builtinPlayableSoundEffect(soundExpression.yawn), music.PlaybackMode.UntilDone)
         servos.P0.stop()
         servos.P2.stop()
+        basic.showLeds(`
+            . . . . .
+            # # . # #
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+        music.play(music.builtinPlayableSoundEffect(soundExpression.yawn), music.PlaybackMode.UntilDone)
     }
 })
 input.onButtonPressed(Button.A, function () {
